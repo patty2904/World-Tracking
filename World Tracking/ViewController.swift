16a73334482899bcceadcd27446c03d9357ed6e5
patty2904 <pattychr29@gmail.com>
2023-10-08 22:24:42 +0100

    Loading from a usdz file from the internet

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -20,21 +20,51 @@ class ViewController: UIViewController {
         self.sceneView.session.run(configuration)
         // Do any additional setup after loading the view.
     }
+    
     @IBAction func add(_ sender: Any) {
-        let node = SCNNode()
-        //Box has firm edges
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
-        //Colour is blue
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        //3D Vector X,Y,Z
-        node.position = SCNVector3(0,0,0)
-        //Scene is whats displaying the camera view of real world
-        //Node is inside scene
-        //Root node has no shape, size or colour
-        //positioned where world origin (starting position)
-        
-        //Node is inside camera view
-        self.sceneView.scene.rootNode.addChildNode(node)
+        print("button has been pressed")
+        // Specify the URL of the USDZ file
+        if let usdzURL = URL(string: "https://developer.apple.com/augmented-reality/quick-look/models/nike-pegasus/sneaker_pegasustrail.usdz") {
+            let task = URLSession.shared.dataTask(with: usdzURL) { (data, response, error) in
+                if let data = data, error == nil {
+                    // Create a temporary file URL to save the downloaded USDZ file
+                    let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                    let tempUSDZURL = documentsURL.appendingPathComponent("temp_usdz_file.usdz")
+                    
+                    do {
+                        try data.write(to: tempUSDZURL)
+                        
+                        let referenceNode = SCNReferenceNode(url: tempUSDZURL)
+                        
+                        // Check if the reference node was successfully created
+                        if let referenceNode = referenceNode {
+                            referenceNode.load()
+                            
+                            // Scale the reference node to make it smaller
+                            let scale = SCNVector3(0.01, 0.01, 0.01) // Adjust the scale as needed
+                            referenceNode.scale = scale
+                            
+                            // Set the position for your USDZ object
+                            referenceNode.position = SCNVector3(0, 0, 0) // Adjust the position as needed
+                            
+                            // Add the reference node to the scene
+                            DispatchQueue.main.async {
+                                self.sceneView.scene.rootNode.addChildNode(referenceNode)
+                            }
+                        } else {
+                            print("Failed to create reference node for USDZ file.")
+                        }
+                    } catch {
+                        print("Error saving USDZ file to a temporary location: \(error)")
+                    }
+                } else {
+                    print("Error downloading the USDZ file: \(error?.localizedDescription ?? "Unknown error")")
+                }
+            }
+            task.resume()
+        } else {
+            print("Invalid URL for the USDZ file.")
+        }
     }
 }
 
