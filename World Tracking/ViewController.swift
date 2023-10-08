eace48b466bfeb3f2c6444e798b0be87b870382b
patty2904 <pattychr29@gmail.com>
2023-10-08 20:57:23 +0100

    Basic world tracking code changes

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    //Tracks orientation
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Debug options
+        //If world was developed correctly
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
         // Do any additional setup after loading the view.
     }
-
-
 }
 
